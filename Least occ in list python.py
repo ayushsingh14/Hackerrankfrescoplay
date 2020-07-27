de7b49891dd0c8f732f7b349135fd6abf0cc5f0e
AYUSH SINGH <40691489+ayushsingh14@users.noreply.github.com>
2020-07-28 02:04:46 +0530

    Update Least occ in list python.py

--- Least occ in list python.py
+++ Least occ in list python.py
@@ -1,3 +1,4 @@
+word="a,bcb,cfd,cfrrrf,cfd,cfd,cfdr,vv,gg,tt,tt,tt,tty,tty,rr,rr,tt,tty"
 word1=word1.split() 
     uniques = []
     for word in word1:
@@ -13,9 +14,10 @@ word1=word1.split()
                  count += 1         # If so, increment the count
         counts.append((count, unique))
 
-    counts=[x[1] for x in counts if x[0]<3]
-    counts.reverse()
+    counts=[x[1] for x in counts if x[0]<3] # contains elements whoose occurance is less than three
+    counts.reverse() # getting the last 20 elemenst firt
     #counts.sort()
-    a=[counts[x] for x in range(len(counts)) if x<20]
+    #a=[counts[x] for x in range(len(counts)) if x<20]
+    a=counts[:20]
     a.reverse()
     print(a)    
